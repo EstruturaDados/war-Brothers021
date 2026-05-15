b3ea3b059a99cb42019969521ac5339298c7c750
Matheus M. <dallalokomatheus07@gmail.com>
2026-05-15 19:41:15 +0000

    removido os buffers

--- nivelnovato.c
+++ nivelnovato.c
@@ -9,12 +9,7 @@ struct territorio {
     char cor[10]; // cor do território
     int tropas; // número de tropas do território 
 };
-// - - função para limpar o buffer de entrada - -
-void limparbufferentrada()
-{
-    int c;
-    while ((c = getchar()) != '\n' && c != EOF);
-}
+
 // - - função principal - - 
 
 int main() {
@@ -25,25 +20,22 @@ int main() {
     printf("\nCadastro de Territórios:\n\n");
 
     // - - O laço será executado a cada 5 vezes, 1 para cada território. 
-    for ( i = 0; i < 5; i++)
-
+    for ( i = 0; i < NUM_TERRITORIOS; i++)
     {
         printf("Território %d:\n", i + 1);
 
         printf("Digite o nome do território: ");
-        fgets(territorios[i].nome, NUM_TERRITORIOS, stdin); // lendo o nome do território
-        scanf("%s", territorios[i].nome); // lendo o nome do território
+        fgets(territorios[i].nome, sizeof territorios[i].nome, stdin);
+        remover_newline(territorios[i].nome);
 
         printf("Digite a cor do território: ");
-        fgets(territorios[i].cor, NUM_TERRITORIOS, stdin); // lendo a cor do território
-        scanf("%s", territorios[i].cor); // lendo a cor do território
+        fgets(territorios[i].cor, sizeof territorios[i].cor, stdin);
+        remover_newline(territorios[i].cor);
 
         printf("Digite o número de tropas:\n ");
-        fgets(territorios[i].tropas,NUM_TERRITORIOS,stdin); // lendo o número de tropas do território
-        scanf("%d", &territorios[i].tropas); // lendo o número de tropas do território 
-
-        limparbufferentrada(); // limpando o buffer de entrada para evitar problemas com fgets
-        printf("\n");
+        if (scanf("%d", &territorios[i].tropas) != 1) {
+            territorios[i].tropas = 0;
+        }
 
     }
         // Aqui irei exibir os dados dos territórios cadastrados 
